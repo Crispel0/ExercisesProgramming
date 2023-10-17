9576fc4ee63a7d57d16b177db83851aaa76509ff
crispelo <penagoscristian61@gmail.com>
2023-10-17 12:18:02 -0500

    exercise getMiddleCharacter resolve with if ternary and substring

--- src/ExerciseBookJava/Abaco.java
+++ src/ExerciseBookJava/Abaco.java
@@ -2,20 +2,20 @@ package ExerciseBookJava;
 
 public class Abaco{
 	 public static void main(String[] args) {
-	        abaco(new String[]{"O---OOOOOOOO", "OOO---OOOOOO", "---OOOOOOOOO", "OO---OOOOOOO", "OOOOOOO---OO", "OOOOOOOOO---", "---OOOOOOOOO"});
+	        abaco(new String[]{"O---OOOOOOOO", "OOO---OOOOOO", "---OOOOOOOOO", "OO---OOOOOOO", 
+	        		"OOOOOOO---OO", "OOOOOOOOO---", "---OOOOOOOOO"});
 	    }
 
 	    private static void abaco(String[] numbers) {
 
 	        String resultado = "";
 
-	        int number = 0;
+	        int numero = 0;
 
 	        for(String n : numbers) {
-
-	            number = n.split("---")[0].length();
+	            numero = n.split("---")[0].length();
 	        
-	            resultado += (number > 0) ? number : (resultado.length() != 0) ? 0 : "";
+	            resultado += (numero > 0) ? numero : (resultado.length() != 0) ? 0 : "";
 	        }
 
 	        System.out.println(resultado);
